44a25f515bb8da47cfe37e606203ea9e51d9ba5c
Charan Teja <96309627+Charantej07@users.noreply.github.com>
2024-01-12 18:24:14 +0530

    Created main.cpp and added the driver code to display the options

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    system("cls");
+    cout << "WELCOME" << endl;
+    cout << endl;
+    int option;
+    while (true)
+    {
+        cout << "SELECT ONE OF THE OPTIONS" << endl;
+        cout << "1.INSERT" << endl;
+        cout << "2.DELETE" << endl;
+        cout << "3.SEARCH" << endl;
+        cout << "4.DISPLAY" << endl;
+        cout << "5.EXIT" << endl;
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        default:
+            cout << "ENTER A VALID OPTION" << endl;
+            break;
+        }
+    }
+}
