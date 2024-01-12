b5751438dcca75873722c1f18fb7165d66030c0e
Charan Teja <96309627+Charantej07@users.noreply.github.com>
2024-01-12 18:42:05 +0530

    Updated main.cpp by creating StorageEngine class to control the operations like insert, search etc

--- main.cpp
+++ main.cpp
@@ -2,7 +2,54 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include "B_Tree.h"
 using namespace std;
+class StorageEngine
+{
+private:
+    BTree *Tree;
+
+public:
+    StorageEngine()
+    {
+        Tree = new BTree;
+    }
+    void insertRecord()
+    {
+        int id;
+        string name;
+        int age;
+        int marks;
+
+        cout << "ENTER THE ID: " << endl;
+        cin >> id;
+        cout << "ENTER THE NAME: " << endl;
+        cin >> name;
+        cout << "ENTER THE AGE: " << endl;
+        cin >> age;
+        cout << "ENTER THE MARKS: " << endl;
+        cin >> marks;
+
+        string recordName = "Records/";
+        recordName += to_string(id) + ".txt";
+        FILE *filePtr = fopen(recordName.c_str(), "w");
+        string data = name + " " + to_string(age) + " " + to_string(marks) + "\n";
+        fprintf(filePtr, data.c_str());
+
+        Key newKey(id, filePtr);
+        Tree->insert(newKey);
+
+        cout << "SUCCESSFULLY INSERTED" << endl;
+        fclose(filePtr);
+    }
+    void searchRecord()
+    {
+        int id;
+        cout << "ENTER THE ID: " << endl;
+        cin >> id;
+        Tree->search(id);
+    }
+};
 int main()
 {
     system("cls");
